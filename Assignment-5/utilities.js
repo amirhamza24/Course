6426a0269c217a3f6c6ff1695fac55b927ac558c
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-02-19 23:46:57 +0600

    Update utilities.js

--- Assignment-5/utilities.js
+++ Assignment-5/utilities.js
@@ -1,25 +1,41 @@
+// find input value
 function getInputValueById(inputId) {
     const input = document.getElementById(inputId);
     const inputString = input.value;
     const inputValue = parseFloat(inputString);
+    input.value = '';
     return inputValue;
 }
 
+// find element title
 function getTextElementValueById(elementId) {
     const title = document.getElementById(elementId);
     const titleValue = title.innerText;
     return titleValue;
 }
 
+// set a table
 function setTableRow(count, titleValue, areaValue,) {
     const parent = document.getElementById('main-table');
     const tr = document.createElement('tr');
     tr.innerHTML = `
-    <td>${count}</td>
+    <td>${count}.</td>
     <td>${titleValue}</td>
-    <td>${areaValue}cm<sup>2</sup></td>
-    <td><button class="bg-sky-500 px-3 py-2 text-white rounded font-semibold">Covert to m<sup>2</sup></button></td>
+    <td>${areaValue} cm<sup>2</sup></td>
+    <td><button class="bg-sky-500 hover:bg-sky-600 px-3 py-2 text-white rounded font-semibold mb-3">Covert to m<sup>2</sup></button></td>
     `;
 
     parent.appendChild(tr);
 }
+
+// set hover effect for every element
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+        color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+} 
+  
+ 
+  
